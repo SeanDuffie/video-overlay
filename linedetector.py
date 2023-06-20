21333f3ece8d7aa020510f3531a507f9faece31b
SeanDuffie <duffiesean@gmail.com>
2023-06-20 16:50:53 -0500

    Major updates and cleanup of linedetector Renamed ZONE constants for better documentation Set up Logger linear_reframe call moved inside compress_columns fixed pyplot labels/ticks added pyplot legend, title, and axis labels removed a large number of "self" calls does min and average at the same time Add instructions for editors

--- linedetector.py
+++ linedetector.py
@@ -1,218 +1,239 @@
 """
 """
-
 import logging
 import os
+import sys
 from tkinter import filedialog
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-# TODO: Logger
-
-OUTPUT_MODE = 0
-ZONE1 = True
-ZONE2 = True
+OUTPUT_MODE: int = 0
+TRIM_FILTER: bool = True        # Do we want to trim the image outside of focus?
+THRESH_FILTER: bool = True      # Do we want to highlight values above a threshold?
 
 class LineDetector:
     """
     """
-    def __init__(self):
-        """
-        """
-        self.filepath, self.img = self.read_file()
+    def __init__(self, filepath = "", img = None):
+        fmt_main = "%(asctime)s | %(levelname)s |\tLineDetector:\t%(message)s"
+        logging.basicConfig(format=fmt_main, level=logging.INFO,
+                        datefmt="%Y-%m-%d %H:%M:%S")
 
-        
-        self.num_cols = len(self.img[0])
+        if img == None:
+            # Get Filepath from user
+            filepath = filedialog.askopenfilename(
+                title="Select Overlay Image",
+                filetypes=[
+                    ("Images", "png"),
+                    ("Images", "jpg"),
+                ]
+            )
+            if filepath == "":
+                logging.error("No Image File Specified! Exiting...")
+                sys.exit(1)
+            logging.info("Processing Image: %s", filepath)
 
-        self.start = 0
-        self.stop = self.num_cols-1
+            # Read in image from user-supplied filepath
+            img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
 
-        self.col_im = self.compress_columns()
-        self.rec_im = self.linear_reframe()
+            self.start: int = 0
+            self.stop: int = len(img[0])
 
-        self.filename = os.path.basename(self.filepath)
+        # Extract the Image name from the path
+        filename: str = os.path.basename(filepath).split(".")[0]
 
-        # x, y = enumerate(col_im[0])
+        # Compress the image into a 1D array of values, then scale to emphasize differences
+        avg_img, min_img = self.compress_columns(img)
+
+        # Plot the arrays to show the user what the distribution is
+        tick_int: int = (self.stop-self.start) // 12
         columns = range(self.start, self.stop)
-        plt.plot(columns, self.col_im[0,self.start:self.stop])
-        plt.plot(columns, self.rec_im[0,self.start:self.stop])
+        column_label = range(self.start, self.stop, tick_int)
+        values = range(0, 256, 16)
+        plt.plot(columns, avg_img, label="Average")
+        plt.plot(columns, min_img, label="Minimum")
 
-        column_label = range(self.start, self.stop, 10)
-        values = range(0, 255, 16)
-        plot_name = f"graph_{self.filename}"
-
+        # Format the Plot and either show or save to PNG
+        plot_name = f"graph_{filename}"
         plt.xlabel("Pixel Columns")
         plt.xticks(ticks=column_label, labels=column_label)
+        plt.ylabel("Value")
         plt.yticks(ticks=values, labels=values)
         plt.title(plot_name)
+        plt.legend(
+            loc="upper left",
+            fancybox=True,
+            shadow=True,
+            ncol=1,
+        )
         # plt.show()
-        plt.savefig(f"graph_{self.filename}")
+        plt.savefig(f"{plot_name}.png")
 
+        # Shut down OpenCV Windows and Pyplot
         # cv2.waitKey()
         plt.close()
         cv2.destroyAllWindows()
 
-
-    def read_file(self):
-        filename = filedialog.askopenfilename()
-        im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-        cv2.imshow("original", im)
-
-        return filename, im
-
-    def compress_columns(self):
-        """ Compress an image to a 1xwidth array
+    def compress_columns(self, img):
+        """ Compress an image to a 1D array of values
         
-            This will take the average of each value in a column, and generate a new image that is
-            only one pixel tall with that
+            This will generate two 1D arrays from the following values
+                - the average of all values in each column
+                - the minumum of all values in each column
         """
-        r, c = self.img.shape
+        r, c = img.shape
         avg_col = np.zeros((r, c), np.uint8)
+        min_col = np.zeros((r, c), np.uint8)
 
         for x in range(c):
-            # avg_col[:,x] = np.average(255-self.img[:,x])
-            avg_col[:,x] = 255-np.min(self.img[:,x])
+            avg_col[:,x] = np.average(img[:,x])
+            min_col[:,x] = np.min(img[:,x])
 
-        if ZONE1:
-            self.select_zone(avg_col)
+        if TRIM_FILTER:
+            start, stop = self.select_zone(avg_col)
+            self.start = start
+            self.stop = stop
+            avg_col = avg_col[0,start:stop]
+            min_col = min_col[0,start:stop]
         else:
-            cv2.imshow("columned", avg_col)
+            avg_col = avg_col[0,:]
+            min_col = min_col[0,:]
 
-        return avg_col
+        avg_col = self.linear_reframe(avg_col)
+        min_col = self.linear_reframe(min_col)
 
-    def select_zone(self, col_img):
+        return avg_col, min_col
+
+    def select_zone(self, col_img) -> tuple[int, int]:
         """
         """
         # color = cv2.cvtColor(col_img, cv2.COLOR_GRAY2BGR)
         first_bar = True
-
         num_cols = len(col_img[0])
-
-        self.start = 0
-        self.stop = num_cols-1
+        start: int = 300
+        stop: int = num_cols-10
 
         # Display usage instructions in terminal
-        print("How to use:")
-        print("\t- 'esc' - skips the current video")
-        print("\t- 'enter' - accepts the current settings")
-        print("\t- 'space' - sets the current frame as the starting point")
-        print("\t- 'backspace' - sets the current frame as the ending point")
-        print("\t- 'left' - moves back one frame")
-        print("\t- 'right' - moves forward one frame")
-        print("\t- 'up' - increases the threshold")
-        print("\t- 'down' - decreases the threshold")
+        logging.info("How to use Trimmer:")
+        logging.info("\t- 'esc' - skips the current video")
+        logging.info("\t- 'enter' - accepts the current settings")
+        logging.info("\t- 'left' - moves back 10 columns")
+        logging.info("\t- 'right' - moves forward 10 columns")
+        logging.info("\t- '1' - Select the Starting column")
+        logging.info("\t- '2' - Select the Stopping column")
 
         # Loop until the user confirms the threshold value from the previews
         while True:
             # Draw lines on image
             color = cv2.cvtColor(col_img, cv2.COLOR_GRAY2BGR)
-            color[:,self.start] = (0,255,0)
-            color[:,self.stop] = (255,0,0)
+            color[:,start] = (0,255,0)
+            color[:,stop] = (0,0,255)
 
             # Show preview
             cv2.imshow("columned", color)
-            # cv2.setMouseCallback('columned', self.click_event)
 
             # Use arrow keys to adjust threshold, up/down are fine tuning, left/right are bigger
             Key = cv2.waitKeyEx()
             if Key == 2424832:          # Left arrow, move bar left
                 if first_bar:
-                    self.start -= 1
-                elif self.stop > self.start+1:
-                    self.stop -= 1
-            elif Key == 2621440:        # Down arrow
-                first_bar = True
-            elif Key == 2490368:        # Up arrow
-                first_bar = False
+                    start -= 10
+                elif stop > start+10:
+                    stop -= 10
             elif Key == 2555904:        # Right arrow, move bar right
                 if not first_bar:
-                    self.stop += 1
-                elif self.start < self.stop-1:
-                    self.start += 1
-            elif Key == 13:             # Enter key, accept current settings
-                self.col_im = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-                break
-            elif Key == 27:
-                self.skip_clip = True
+                    stop += 10
+                elif start < stop-10:
+                    start += 10
+            elif Key == 49:        # 1 Key
+                first_bar = True
+            elif Key == 50:        # 2 Key
+                first_bar = False
+            elif Key == 13:             # Enter Key, accept current settings
+                return start, stop
+            elif Key == 27:             # Escape Key
                 logging.info("Skipping video...")
-                break
+                return 0, num_cols-10
             else:
                 logging.warning("Invalid Key: %d", Key)
 
             # Enforce bounds and debug
-            if self.stop >= num_cols:
-                self.stop = num_cols-1
-            if self.start < 0:
-                self.start = 0
+            if stop >= num_cols:
+                stop = num_cols-1
+            if start < 0:
+                start = 0
 
-    def linear_reframe(self):
+    def linear_reframe(self, img):
         """ Using a 1D array, it will readjust the value scale to make differences more noticable
         
             This will be done by finding the min and max values, subtracting the min, and
             multiplying by the different between the two scaled to 255.
         """
-        b_p = np.min(self.col_im[0])
-        w_p = np.max(self.col_im[0])
+        if THRESH_FILTER:
+            img = self.highlight(img)
+
+        b_p = np.min(img)
+        w_p = np.max(img)
 
         scale = (w_p-b_p)/255
-        rec_img = np.clip(self.col_im, b_p, w_p) - b_p
+        rec_img = np.clip(img, b_p, w_p) - b_p
         rec_img = (rec_img / scale).astype(np.uint8)
-
-        if ZONE2:
-            rec_img = self.highlight(rec_img)
-        else:
-            cv2.imshow("scaled", rec_img)
 
         return rec_img
 
-    def highlight(self, rec_img):
+    def highlight(self, img):
         """
         """
-        # color = cv2.cvtColor(rec_img, cv2.COLOR_GRAY2BGR)
-        r, c = self.img.shape
-        first_bar = True
-        thresh = 127
+        thresh: int = 255
+
+        # Display usage instructions in terminal
+        logging.info("How to use Highlighter:")
+        logging.info("\t- 'esc' - skips the current video")
+        logging.info("\t- 'enter' - accepts the current settings")
+        logging.info("\t- 'up' - increases the threshold")
+        logging.info("\t- 'down' - decreases the threshold")
 
         # Loop until the user confirms the threshold value from the previews
         while True:
             # Draw lines on image
-            color = cv2.cvtColor(rec_img, cv2.COLOR_GRAY2BGR)
+            # color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            # mkd_img = np.minimum(img, thresh)
+            mkd_img = np.copy(img)
 
-            for x in range(c):
-                if rec_img[0,x] > thresh:
-                    color[:,x] = (0,255,0)
-            # color[:,self.start] = (0,255,0)
-            # color[:,self.stop] = (255,0,0)
+            # for x in range(c):
+            #     if color[0,x] > thresh:
+            #         color[:,x] = (0,255,0)
+
+            for x in range(len(mkd_img)):
+                if mkd_img[x] > thresh:
+                    mkd_img[x] = 0
+
 
             # Show preview
-            cv2.imshow("scaled", color)
-            # cv2.setMouseCallback('scaled', self.click_event)
+            h = 30
+            mkd_img_2d = np.repeat(mkd_img, h).reshape((h, len(mkd_img)))
+            cv2.imshow("scaled", mkd_img_2d)
 
             # Use arrow keys to adjust threshold, up/down are fine tuning, left/right are bigger
             Key = cv2.waitKeyEx()
-            # if Key == 2424832:          # Left arrow, move bar left
-            if Key == 2621440:        # Down arrow
-                thresh -= 1
+            if Key == 2621440:          # Down arrow
+                if thresh > 0:      # Enforce min bounds
+                    thresh -= 1
+                logging.info("Threshold = %d", thresh)
             elif Key == 2490368:        # Up arrow
-                thresh += 1
-            # elif Key == 2555904:        # Right arrow, move bar right
+                if thresh < 255:    # Enforce max bounds
+                    thresh += 1
+                logging.info("Threshold = %d", thresh)
             elif Key == 13:             # Enter key, accept current settings
-                rec_im = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-                return rec_im
+                logging.info("Threshold value set to %d...", thresh)
+                # img = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+                return mkd_img
             elif Key == 27:
-                self.skip_clip = True
-                logging.info("Skipping video...")
-                break
+                logging.info("Skipping Image Highlight...")
+                return img
             else:
                 logging.warning("Invalid Key: %d", Key)
-
-            # Enforce bounds and debug
-            if thresh > 255:
-                thresh = 255
-            if thresh < 0:
-                thresh = 0
 
     def mountain_climber(self):
         """
