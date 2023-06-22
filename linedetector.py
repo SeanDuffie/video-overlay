78cb3e5a963962d6d9f6071365a4dccc21e58dfe
SeanDuffie <duffiesean@gmail.com>
2023-06-22 13:43:03 -0500

    Added basic Mountain climber! Creates an output image with lines drawn over clusters Adds blur to the image for noise reduction Uses the whole image instead of trimming No longer using threshold/highlight

--- linedetector.py
+++ linedetector.py
@@ -41,11 +41,28 @@ class LineDetector:
             # Extract the Image name from the path
             filename: str = os.path.basename(filepath).split(".")[0]
 
-        self.start: int = 350
-        self.stop: int = len(img[0])-12
+        self.start: int = 0#350
+        self.stop: int = len(img[0])#-12
 
-        # Compress the image into a 1D array of values, then scale to emphasize differences
-        avg_img, min_img = self.compress_columns(img)
+        # This will contain a list of peaks and valleys
+        self.cluster_list = []
+
+        # Blur, then Compress the image into a 1D array of values, then scale to emphasize differences
+        blur = cv2.GaussianBlur(img, (15, 5), 0)
+        avg_img = self.compress_columns(blur)
+        color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        print(self.cluster_list)
+        mk = np.zeros((len(avg_img)), np.uint8)
+        for i, val, climb in self.cluster_list:
+            plt.plot(i, val, "og")
+            if val < 127:
+                color[:,i] = (0,0,255)
+            elif climb > 0:
+                color[:,i] = (255,0,0)
+            else:
+                color[:,i] = (0,255,0)
+        cv2.imshow("marked", color)
+        cv2.imwrite(f"{filename}_colored.png", color)
 
         # Plot the arrays to show the user what the distribution is
         tick_int: int = (self.stop-self.start) // 12
@@ -53,7 +70,6 @@ class LineDetector:
         column_label = range(self.start, self.stop, tick_int)
         values = range(0, 256, 16)
         plt.plot(columns, avg_img, label="Average")
-        plt.plot(columns, min_img, label="Minimum")
 
         # Format the Plot and either show or save to PNG
         plt.xlabel("Pixel Columns")
@@ -82,32 +98,24 @@ class LineDetector:
                 - the average of all values in each column
                 - the minumum of all values in each column
         """
-        r, c = img.shape
-        avg_col = np.zeros((r, c), np.uint8)
-        min_col = np.zeros((r, c), np.uint8)
-
-        for x in range(c):
-            avg_col[:,x] = np.average(img[:,x])
-            min_col[:,x] = np.min(img[:,x])
-
         if TRIM_FILTER:
-            start, stop = self.select_zone(avg_col)
-            self.start = start
-            self.stop = stop
+            self.start, self.stop = self.select_zone(img)
+
+        avg_col = np.zeros((self.stop-self.start), np.uint8)
+        for x in range(len(avg_col)):
+            avg_col[x] = np.average(img[:,x])
 
         # Rescale the images, trim to new start/stop, and convert to 1D
-        avg_col = self.linear_reframe(avg_col[0,self.start:self.stop])
-        min_col = self.linear_reframe(min_col[0,self.start:self.stop])
+        avg_col = self.linear_reframe(avg_col)
 
-        return avg_col, min_col
+        return avg_col
 
-    def select_zone(self, col_img) -> tuple[int, int]:
+    def select_zone(self, img) -> tuple[int, int]:
         """
         """
-        # color = cv2.cvtColor(col_img, cv2.COLOR_GRAY2BGR)
         first_bar = True
-        num_cols = len(col_img[0])
-        start: int = 300
+        num_cols = len(img[0])
+        start: int = 350
         stop: int = num_cols-10
 
         # Display usage instructions in terminal
@@ -122,12 +130,12 @@ class LineDetector:
         # Loop until the user confirms the threshold value from the previews
         while True:
             # Draw lines on image
-            color = cv2.cvtColor(col_img, cv2.COLOR_GRAY2BGR)
+            color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
             color[:,start] = (0,255,0)
             color[:,stop] = (0,0,255)
 
             # Show preview
-            cv2.imshow("columned", color)
+            cv2.imshow("markers", color)
 
             # Use arrow keys to adjust threshold, up/down are fine tuning, left/right are bigger
             Key = cv2.waitKeyEx()
@@ -143,7 +151,7 @@ class LineDetector:
                     start += 10
             elif Key == 49:        # 1 Key
                 first_bar: bool = True
-                logging.info("Now changing the Staring point...")
+                logging.info("Now changing the Starting point...")
             elif Key == 50:        # 2 Key
                 first_bar: bool = False
                 logging.info("Now changing the Stopping point...")
@@ -179,14 +187,8 @@ class LineDetector:
         # If we want to manually filter out certain values, this will prompt us
         if THRESH_FILTER:
             rec_img = self.highlight(rec_img)
-        
-            # Second round of linear_reframing, this is unnecessary but helpful if highlight performs a significant change
-            b_p = np.min(rec_img)
-            w_p = np.max(rec_img)
 
-            scale = (w_p-b_p)/255
-            rec_img = np.clip(rec_img, b_p, w_p) - b_p
-            rec_img = (rec_img / scale).astype(np.uint8)
+        self.cluster_list = self.mountain_climber(rec_img)
 
         return rec_img
 
@@ -202,27 +204,26 @@ class LineDetector:
         logging.info("\t- 'up' - increases the threshold")
         logging.info("\t- 'down' - decreases the threshold")
 
+        # TODO: make thresh automatic
         thresh: int = 255
 
         # Loop until the user confirms the threshold value from the previews
         while True:
-            # Draw lines on image
-            # color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-            # mkd_img = np.minimum(img, thresh)
             mkd_img = np.copy(img)
 
-            # for x in range(c):
-            #     if color[0,x] > thresh:
-            #         color[:,x] = (0,255,0)
-
-            for x in range(len(mkd_img)):
-                if mkd_img[x] > thresh:
-                    mkd_img[x] = 0
+            # Cut noise to floor
+            # for x in range(len(mkd_img)):
+            #     if mkd_img[x] > thresh:
+            #         mkd_img[x] = 0
+            #     else:
+            #         mkd_img[x] = 255
 
 
             # Convert the 1D array into a 2D image and display it
+            img_2d = np.stack([img for _ in range(100)], axis=0)
             mkd_img_2d = np.stack([mkd_img for _ in range(100)], axis=0)
-            cv2.imshow("scaled", mkd_img_2d)
+            cv2.imshow("original", img_2d)
+            cv2.imshow("averaged", mkd_img_2d)
 
             # Use arrow keys to adjust threshold, up/down are fine tuning, left/right are bigger
             Key = cv2.waitKeyEx()
@@ -236,17 +237,82 @@ class LineDetector:
                 logging.info("Threshold = %d", thresh)
             elif Key == 13:             # Enter key, accept current settings
                 logging.info("Threshold value set to %d...", thresh)
-                # img = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
                 return mkd_img
             elif Key == 27:
                 logging.info("Skipping Image Highlight...")
                 return img
+            elif Key == -1:
+                pass
             else:
                 logging.warning("Invalid Key: %d", Key)
 
-    def mountain_climber(self):
+    def mountain_climber(self, avg_img, minClimb: int = 0):
         """
         """
+        def getDirection(a: int, b: int) -> int:
+            dx: int = b - a
+            if dx == 0:
+                return 0
+            elif dx > 0:
+                return -1
+            return 1
+
+        # recent_Valley: int = avg_img[0]
+        # recent_Peak: int = avg_img[0]
+        # recent_Direction: int = 0
+        # lastValley: int = avg_img[0]
+
+        # wall_thresh: int = 128
+
+        # wall_loc1: int = -1
+        # wall_loc2: int = -1
+
+        # for col, val in enumerate(avg_img):
+        #     recent
+
+        #     if val >= wall_thresh:
+        #         if 
+
+        # StackOverflow     # init trackers
+        last_valley: int = int(avg_img[0])
+        last_peak: int = int(avg_img[0])
+        last_val: int = int(avg_img[0])
+        last_dir: int = getDirection(int(avg_img[0]), int(avg_img[1]))
+
+        # get climbing
+        peak_valley = [] # index, height, climb (positive for peaks, negative for valleys)
+        for a in range(1, len(avg_img)):
+            # get current direction
+            sign = getDirection(last_val, int(avg_img[a]))
+            last_val = int(avg_img[a])
+
+            # if not equal, check gradient
+            if sign != 0:
+                if sign != last_dir:
+                    # change in gradient, record peak or valley
+                    # peak
+                    if last_dir > 0:
+                        last_peak = int(avg_img[a])
+                        climb = last_peak - last_valley
+                        climb = round(climb, 2)
+                        peak_valley.append([a, int(avg_img[a]), climb])
+                    else:
+                        # valley
+                        last_valley = int(avg_img[a])
+                        climb = last_valley - last_peak
+                        climb = round(climb, 2)
+                        peak_valley.append([a, int(avg_img[a]), climb])
+
+                    # change direction
+                    last_dir = sign
+
+        # filter out very small climbs
+        filtered_pv = []
+        for dot in peak_valley:
+            if abs(dot[2]) > minClimb:
+                filtered_pv.append(dot)
+        return filtered_pv
+
 
 if __name__ == "__main__":
     LineDetector()
